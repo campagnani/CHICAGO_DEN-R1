3566411b8f752a5b59993dbea5616b4683fa984e
Thalles Campagnani <thallescampagnani@gmail.com>
2024-03-08 04:06:57 -0300

    Configuração máxima MPI

--- CodeOpenMC/testeMPI.py
+++ CodeOpenMC/testeMPI.py
@@ -10,12 +10,13 @@
 import libChicagoDenR1
 
 chicago = libChicagoDenR1.ChigagoDenR1(material="u_nat", particulas=100000, ciclos=400, inativo=40, fonte=False)
-chicago.run(nodesMPI=24, hostfile="hostfile.txt",)
-chicago.run(nodesMPI=24, hosts=[
-    "nuclear215",
-    "nuclear214",
-    "nuclear213",
-    "nuclear212",
-    "nuclear206",
-    "nuclear205",
+chicago.run(nodesMPI=52, hostfile="hostfile.txt",)
+chicago.run(nodesMPI=52, hosts=[
+    #HOST,        #CORES
+    "nuclear215", #8
+    "nuclear214", #8
+    "nuclear213", #8
+    "nuclear212", #6
+    "nuclear206", #16
+    "nuclear205", #8
     ])
